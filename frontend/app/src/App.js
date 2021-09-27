0539901905ad1f400f65d52035c03e7e8f034414
irvintingsieze <irvintingg@gmail.com>
2021-09-27 23:22:55 +0800

    updates

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -5,20 +5,31 @@ import axios from "axios";
 
 function App() {
   const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const res = axios.get("http://localhost:5000/get_all");
+
+  const getUserList = async () => {
+    const res = await axios.get("http://localhost:5000/get_all");
+    const resp = await axios.post("http://localhost:5000/login",{
+      "name":"Jim",
+      "password":"JimPW"
+  });
+  console.log(resp.data);
+    console.log(res);
     console.log(res.data);
     setUsers(res.data);
+  };
+  useEffect(() => {
+    getUserList();
   }, []);
   return (
     <div className="App">
+    <h1>HELLO</h1>
       {users &&
-        users.map((user) => {
+        users.map((user) => (
           <div>
             <div>Name: {user.name}</div>
             <div>password: {user.password}</div>
-          </div>;
-        })}
+          </div>
+        ))}
     </div>
   );
 }
